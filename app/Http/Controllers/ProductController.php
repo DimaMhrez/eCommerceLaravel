08f96088d2025e05e1f853a0734fc129c19d8bd9
Marco Ovidi <marco.ovidi.96@gmail.com>
2019-02-22 18:53:52 +0100

    migliore tabella prodotti back_end

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -143,9 +143,40 @@ class ProductController extends Controller
     public function edit($id)
     {
         // get the product
-        $product = Product::find($id);
-        $bullets = BulletDescription::where('')
 
+        $item = Product::find($id);
+        $brand = Brand::find($item->brand_id);
+        $category = Category::find($item->category_id);
+
+        //prelevo i bullet description dato $id che è l'$id del prodotto passato alla funzione edit
+        $bulletDescription = BulletDescription::where('product_id', $id)->orderBy('id', 'asc')->get();
+
+
+        //creo un array con tutti i dati necessari per il form
+        $product = array('id'=>$item->id,
+                         'name'=>$item->name,
+                         'description'=>$item->description,
+                         'brand'=>$brand->name,
+                         'basicPrice'=>$item->normalPrice,
+                         'category'=>$category->name,
+                         'showcase'=>$item->showcase,
+                         'featured'=>$item->featured,
+                         'special'=>$item->special,
+                         'bullet1'=>null,
+                         'bullet2'=>null,
+                         'bullet3'=>null,
+                         'bullet4'=>null,
+                         'bullet5'=>null,
+                         'bullet6'=>null);
+
+        $count = 1;
+
+
+        //per ogni bullet d. voglio mettere in bullet.$count (ovvero bullet1 , bullet2 ...) i valori del campo description del bullet
+        foreach ($bulletDescription as $bd) {
+            $product['bullet' . $count] = $bd->description;
+            $count++;
+        }
         // show the edit form and pass the nerd
         return View::make('back_end.editProduct')
             ->with('product', $product);
@@ -160,7 +191,91 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = array(
+            'name'       => 'required',
+            'brand'      => 'required',
+            'basicPrice' => 'required|numeric',
+            'category'   => 'required'
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return Redirect::to('/admin/product/create')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+
+        } else {
+            $product = Product::find($id);
+
+
+            $brandExist = Brand::where('name',Input::get('brand'))->select('id')->first();
+
+
+            if($brandExist != null){
+                $product->brand_id = $brandExist->id;
+            }else{
+                $brand = new Brand();
+
+                $brand->name = Input::get('brand');
+
+                $brand->save();
+
+                $product->brand_id = $brand->id;
+            }
+
+            $product->name = Input::get('name');
+            $product->description = Input::get('fulldescription');
+            $product->normalprice = Input::get('basicPrice');
+
+
+            if(Input::get('showcase')){
+                $product->showcase = 1;
+            }else{
+                $product->showcase = 0;
+            }
+
+            if(Input::get('featured')){
+                $product->featured  = 1;
+            }else{
+                $product->featured  = 0;
+            }
+
+            if(Input::get('special')){
+                $product->special = 1;
+            }else{
+                $product->special = 0;
+            }
+
+
+            $category = Category::where('id',Input::get('category'))->select('id')->first();
+
+            $product->category_id = $category->id;
+
+
+            $product->save();
+
+
+            $bulletDescription = BulletDescription::where('product_id', $id)->orderBy('id', 'asc')->get();
+
+            foreach ($bulletDescription as $bd){
+                $bd->delete();
+            }
+
+
+            for ($i = 1; $i <= 7; $i++) {
+                if(!empty(Input::get('bullet'.$i))){
+                    $bullet = new BulletDescription();
+
+                    $bullet->description = Input::get('bullet'.$i);
+                    $bullet->product_id = $product->id;
+                    $bullet->save();
+                }
+            }
+
+
+            return Redirect::to('/admin/product/');
+        }
     }
 
     /**
@@ -186,7 +301,7 @@ class ProductController extends Controller
             //->addColumn('intro', '<a href="{{ url(\'admin/userProfile/'.id.'\') }}" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>')
 
             ->addColumn('intro', function(Product $product) {
-                return '<a href="'. url('/admin/') .'" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>';
+                return '<a href="'. url('/admin/product/'.$product->id.'/edit') .'" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">edit</i></a>';
             })
             ->rawColumns(['intro'])
             ->toJson();
